41c2be36f0a3ab20eebaae768ea13e719cb473dd
nrholm1 <nielsraunkjaer@gmail.com>
2020-12-30 22:14:41 +0100

    Refactor and improve UI in regards to offset

--- mobile-client/components/DragDropList.tsx
+++ mobile-client/components/DragDropList.tsx
@@ -5,149 +5,9 @@ import ExerciseCard from './ExcerciseCard';
 import { Exercise } from '../models/Exercise';
 import { View } from "./Themed";
 import { FlatList, Text, PanResponder, PanResponderInstance, Animated } from "react-native";
+import __testExercises from "./__testExercises";
 
-const _exercises: Exercise[] = [
-    {
-        id: 1,
-        name: "Bench Press",
-        description: "Press on bench - for pecs",
-        exerciseType: "Compound",
-        difficulty: 5
-    },
-    {
-        id: 2,
-        name: "Squat",
-        description: "Sit down with a bar on the shoulders - for legs",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 3,
-        name: "Deadlift",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 4,
-        name: "Pullup",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 5,
-        name: "Pushup",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 6,
-        name: "Bench Press",
-        description: "Press on bench - for pecs",
-        exerciseType: "Compound",
-        difficulty: 5
-    },
-    {
-        id: 7,
-        name: "Squat",
-        description: "Sit down with a bar on the shoulders - for legs",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 8,
-        name: "Deadlift",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 9,
-        name: "Pullup",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 10,
-        name: "Pushup",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 11,
-        name: "Bench Press",
-        description: "Press on bench - for pecs",
-        exerciseType: "Compound",
-        difficulty: 5
-    },
-    {
-        id: 12,
-        name: "Squat",
-        description: "Sit down with a bar on the shoulders - for legs",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 13,
-        name: "Deadlift",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 14,
-        name: "Pullup",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 15,
-        name: "Pushup",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 16,
-        name: "Bench Press",
-        description: "Press on bench - for pecs",
-        exerciseType: "Compound",
-        difficulty: 5
-    },
-    {
-        id: 17,
-        name: "Squat",
-        description: "Sit down with a bar on the shoulders - for legs",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 18,
-        name: "Deadlift",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 19,
-        name: "Pullup",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    },
-    {
-        id: 20,
-        name: "Pushup",
-        description: "Straight, rigid back, lift bar up and down - for back",
-        exerciseType: "Compound",
-        difficulty: 6
-    }
-];
+const _exercises: Exercise[] = __testExercises.exercises;
 
 const colorMap: string[] = [];
 
@@ -165,12 +25,17 @@ export default class DragDropList extends React.Component {
             colorMap[i] = getRandomColor();
             return i;
         }),
-        dragging: true
+        dragging: true,
+        draggingIndex: 0
     };
 
     _panResponder: PanResponderInstance;
     point = new Animated.ValueXY();
     scrollOffset = 0;
+    elementOffset = 90;     // hardcoded, but should be dynamic :)
+    flatlistTopOffset = 0;  
+    rowHeight = 0;
+    currentIdx = -1;
 
     constructor(props: any) {
         super(props);
@@ -185,17 +50,17 @@ export default class DragDropList extends React.Component {
       
             onPanResponderGrant: (evt, gestureState) => {
                 console.log(gestureState.y0);
-                this.setState({ dragging: true });
+                this.currentIdx = this.yToIndex(gestureState.y0);
+                this.setState({ dragging: true, draggingIndex: this.currentIdx });
             },
             onPanResponderMove: (evt, gestureState) => {
-                const offset: number = 50;
-                this.point.setOffset({x: 0, y: -offset}); // hardcoded, but should be dynamic :)
+                this.point.setOffset({x: 0, y: -this.elementOffset});
 
                 let moveY;
-                if (gestureState.moveY - offset >= 0)
+                if (gestureState.moveY - this.elementOffset >= 0)
                     moveY = gestureState.moveY;
                 else
-                    moveY = offset;
+                    moveY = this.elementOffset;
 
                 Animated.event([{y: this.point.y}],
                     {useNativeDriver: false})
@@ -215,23 +80,33 @@ export default class DragDropList extends React.Component {
           })
     }
 
+    yToIndex = (y: number) => {
+        return Math.floor(
+            (this.scrollOffset + y + this.flatlistTopOffset) 
+            / this.rowHeight) - 2;
+    }
+
     componentDidMount(): void {
         // make initial graphical state deterministic -> "folds out" list immediately instead of having to drag manually
         setTimeout(() => this.resetDrag(), 3);
     }
 
     resetDrag = () => {
-        this.setState({ dragging: false });
+        this.setState({ dragging: false, draggingIndex: -1 });
     }
 
     render() {
-        const { data, dragging } = this.state;
+        const { data, dragging, draggingIndex } = this.state;
 
-        const renderItem = ({ item }: any) => (
-            <View style={{
-                backgroundColor: colorMap[item.id - 1],
-                padding: 3,
-                flexDirection: "row"
+        const renderItem = ({ item, index }: any) => (
+            <View 
+                onLayout={e => {this.rowHeight 
+                                = e.nativeEvent.layout.height}} 
+                style={{
+                    backgroundColor: colorMap[item.id - 1],
+                    padding: 3,
+                    flexDirection: "row",
+                    opacity: draggingIndex === index ? 0 : 1
                 }}>
                 <View {...this._panResponder.panHandlers}
                 style={{
@@ -249,19 +124,21 @@ export default class DragDropList extends React.Component {
         );
 
         return (
-            <View style={{width:"100%", minHeight: "100%"}}>
+            <View style={{width:"100%"}}>
                 {dragging && <Animated.View style={{ backgroundColor: "black", 
                                         zIndex: 2,
+                                        position: "absolute",
+                                        width: "100%",
                                         top: this.point.getLayout().top}}>
-                    {renderItem({item:  _exercises[2]})}
+                    {renderItem({item:  _exercises[draggingIndex]})}
                 </Animated.View>}
 
                 <FlatList
                     scrollEnabled={!dragging}
                     data={_exercises}
-                    onScroll={e => 
-                        this.scrollOffset = e.nativeEvent.contentOffset.y
-                    }
+                    onScroll={e => this.scrollOffset 
+                                   = e.nativeEvent.contentOffset.y}
+                    onLayout={e => this.flatlistTopOffset = e.nativeEvent.layout.y}
                     scrollEventThrottle={16}
                     renderItem={renderItem}
                     keyExtractor={(item) => item.id.toString()}
---
+++ mobile-client/components/__testExercises.ts
@@ -0,0 +1,286 @@
+import { Exercise } from "../models/Exercise";
+
+export default class __testExercises {
+    static exercises : Exercise[] = [
+        {
+            id: 1,
+            name: "Bench Press",
+            description: "Press on bench - for pecs",
+            exerciseType: "Compound",
+            difficulty: 5
+        },
+        {
+            id: 2,
+            name: "Squat",
+            description: "Sit down with a bar on the shoulders - for legs",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 3,
+            name: "Deadlift",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 4,
+            name: "Pullup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 5,
+            name: "Pushup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 6,
+            name: "Bench Press",
+            description: "Press on bench - for pecs",
+            exerciseType: "Compound",
+            difficulty: 5
+        },
+        {
+            id: 7,
+            name: "Squat",
+            description: "Sit down with a bar on the shoulders - for legs",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 8,
+            name: "Deadlift",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 9,
+            name: "Pullup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 10,
+            name: "Pushup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 11,
+            name: "Bench Press",
+            description: "Press on bench - for pecs",
+            exerciseType: "Compound",
+            difficulty: 5
+        },
+        {
+            id: 12,
+            name: "Squat",
+            description: "Sit down with a bar on the shoulders - for legs",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 13,
+            name: "Deadlift",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 14,
+            name: "Pullup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 15,
+            name: "Pushup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 16,
+            name: "Bench Press",
+            description: "Press on bench - for pecs",
+            exerciseType: "Compound",
+            difficulty: 5
+        },
+        {
+            id: 17,
+            name: "Squat",
+            description: "Sit down with a bar on the shoulders - for legs",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 18,
+            name: "Deadlift",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 19,
+            name: "Pullup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 20,
+            name: "Pushup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 21,
+            name: "Bench Press",
+            description: "Press on bench - for pecs",
+            exerciseType: "Compound",
+            difficulty: 5
+        },
+        {
+            id: 22,
+            name: "Squat",
+            description: "Sit down with a bar on the shoulders - for legs",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 23,
+            name: "Deadlift",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 24,
+            name: "Pullup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 25,
+            name: "Pushup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 26,
+            name: "Bench Press",
+            description: "Press on bench - for pecs",
+            exerciseType: "Compound",
+            difficulty: 5
+        },
+        {
+            id: 27,
+            name: "Squat",
+            description: "Sit down with a bar on the shoulders - for legs",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 28,
+            name: "Deadlift",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 29,
+            name: "Pullup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 30,
+            name: "Pushup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 31,
+            name: "Bench Press",
+            description: "Press on bench - for pecs",
+            exerciseType: "Compound",
+            difficulty: 5
+        },
+        {
+            id: 32,
+            name: "Squat",
+            description: "Sit down with a bar on the shoulders - for legs",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 33,
+            name: "Deadlift",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 34,
+            name: "Pullup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 35,
+            name: "Pushup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 36,
+            name: "Bench Press",
+            description: "Press on bench - for pecs",
+            exerciseType: "Compound",
+            difficulty: 5
+        },
+        {
+            id: 37,
+            name: "Squat",
+            description: "Sit down with a bar on the shoulders - for legs",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 38,
+            name: "Deadlift",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 39,
+            name: "Pullup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        },
+        {
+            id: 40,
+            name: "Pushup",
+            description: "Straight, rigid back, lift bar up and down - for back",
+            exerciseType: "Compound",
+            difficulty: 6
+        }
+    ];
+}
