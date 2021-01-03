254292999de8a8282ed31630f5e108bf9f2d5c74
nrholm1 <nielsraunkjaer@gmail.com>
2021-01-03 20:33:17 +0100

    Add Update and Delete methods in service - plan is to use this as template for other services, therefore it is as general as possible

--- api/Services/ExerciseService.cs
+++ api/Services/ExerciseService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using ExerciseAPI.Entities;
 using ExerciseAPI.Utils;
@@ -45,14 +46,31 @@ namespace ExerciseAPI.Services
             throw new AppException($"No Exercise with ID {id} found.");
         }
 
-        public async Task<Exercise> Update(Exercise entityParam)
+        public async Task<Exercise> Update(int id, Exercise entityParam)
         {
-            throw new NotImplementedException();
+            var entity = await _context.Exercises.FindAsync(id);
+            if (entity != null)
+                // loop over each property of entity and update in db if any of them have changed
+                foreach(var prop in entity.GetType()
+                                                   .GetProperties())
+                    if (prop.GetValue(entityParam, null) 
+                        != prop.GetValue(entity, null))
+                        prop.SetValue(entity, entityParam);
+
+            await _context.SaveChangesAsync();
+            
+            return entity;
         }
 
         public async Task Delete(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _context.Exercises.FindAsync(id);
+
+            if (entity == null) 
+                throw new AppException($"Exercise with id {id} was not found.");
+        
+            _context.Exercises.Remove(entity);
+            await _context.SaveChangesAsync();
         }
     }
 }
